13b5e677132e5b3eb74fb2b11d072c5f27e5bc39
Dmytro Fomin <78318568+Fodi999@users.noreply.github.com>
2024-07-27 13:10:01 +0200

    header fix

---
+++ public/service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'cms-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles.css',
+  '/manifest.json',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/icons/apple-touch-icon.png',
+  '/icons/safari-pinned-tab.svg'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+
